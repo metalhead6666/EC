e84919911a573c9b2bc825a9e04152817d5b0c75
metalhead_6666 <noe.godinho.92@gmail.com>
2016-04-20 23:16:26 +0100

    ttp script still in work

---
+++ ttp.py
@@ -0,0 +1,4 @@
+from file_parser import file_parser
+
+def stuff():
+	numCities, numItems, knapsackWeight, vMax, vMin, coefficient, dropRate, distanceMatrix, weightValueItems, availabilityItems = file_parser('Tests/10/10_3_1_25.txt')
