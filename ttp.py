b89fd7b2f9bfd1a4f7dd23266fbf5907f2f9a0a4
metalhead_6666 <noe.godinho.92@gmail.com>
2016-04-25 18:58:13 +0100

    Updated pop creation

--- ttp.py
+++ ttp.py
@@ -1,39 +1,27 @@
 #! /usr/bin/env python3
 
 from file_parser import fileParser
+from random import randint, sample
 
-def main(filename, probMutation, probCrossover, numRuns):
+def main(filename, probMutation, probCrossover, numRuns, popSize):
 	numCities, numItems, knapsackWeight, vMax, vMin, coefficient, dropRate, distanceMatrix, weightValueItems, availabilityItems = fileParser(filename)
 	
 	# item list in which position is item_i and the value is the city where it was picked
-	items = initialization(numItems)
+	itemsPop = initialization_items(numItems, numCities, popSize)
 
-	# item time list which defines the time where item_i was picked -> necessary to g(x,z)
-	itemsTime = initialization(numItems)
-
-	# time passed since the start of the trip
-	timePassed = 0
-
-	# actual speed, which is between vMin and vMax
-	vActual = vMax
-
-	# actual knapsack weight, which is between 0 and knapsackWeight
-	actualKnapsackWeight = 0
+	citiesPop = initialization_cities(numCities, popSize)
+	#print(citiesPop)
 	
-def initialization(num):
-	return [0]*num
+def initialization_items(numItems, numCities, popSize):
+	return [[randint(1, numCities) for i in range(numItems)] for i in range(popSize)]
 
-def mutation(items, probability):
-	return items
+def initialization_cities(numCities, popSize):	
+	return [sample(list(range(1, numCities + 1)), numCities) for i in range(popSize)]
 
-def crossover(items, probability):
-	return items
-
-def fitness(items, dropRate, vMax, vMin):
-	return 0
 
 if __name__ == '__main__':
-	probMutation = 0.01
+	probMutation = 0.1
 	probCrossover = 0.9
-	numRuns = 30	
-	main('Tests/10/10_3_1_25.txt', probMutation, probCrossover, numRuns)
+	numRuns = 30
+	popSize = 10	
+	main('Tests/10/10_3_1_25.txt', probMutation, probCrossover, numRuns, popSize)
