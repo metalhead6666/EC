0d681dfb9eebb883ffdc8c9b6c217a492b67d617
metalhead_6666 <noe.godinho.92@gmail.com>
2016-05-23 18:51:43 +0100

    File data parser

--- Multipopulation/file_parser.py
+++ Multipopulation/file_parser.py
@@ -6,10 +6,10 @@ def file_parser(file_number):
 	file_weights = 'p0' + str(file_number) + '_w.txt'
 	file_profits = 'p0' + str(file_number) + '_p.txt'
 
-	with open(file_capacity, 'r') as f:
+	with open(folder + file_capacity, 'r') as f:
 		knapsack_capacity = int(f.readline())
 
-	with open(file_weights, 'r') as f:
+	with open(folder + file_weights, 'r') as f:
 		temp_list = []
 		number_objects = 0
 
@@ -17,14 +17,14 @@ def file_parser(file_number):
 			temp_list.append(int(line))
 			number_objects += 1
 
-		knapsack_weights = np.array(temp_list)
+		objects_weight = np.array(temp_list)
 
-	with open(file_profits, 'r') as f:
+	with open(folder + file_profits, 'r') as f:
 		temp_list = []
 
 		for line in f:
 			temp_list.append(int(line))
 
-		knapsack_profits = np.array(temp_list)
+		objects_profit = np.array(temp_list)
 
 	return knapsack_capacity, number_objects, objects_weight, objects_profit
